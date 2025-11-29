caa0b7817ad308f7be05b274b9f700327653907d
Emile 'iMil' Heitor <imil@NetBSD.org>
2025-11-29 11:26:24 +0100

    more tools added and PS1 style

--- service/usershell/postinst/custom.sh
+++ service/usershell/postinst/custom.sh
@@ -3,5 +3,5 @@
 rm -f etc/shrc # wipe defaults
 cat >>etc/profile<<EOF
 HOSTNAME=\$(hostname)
-#PS1="\$(printf '\e[1;31m\${USER}\e[1;37m@\${HOSTNAME}\e[0m$ ')"
+PS1="\$(printf '\e[1;31m\${USER}\e[1;37m@\${HOSTNAME}\e[0m$ ')"
 EOF
